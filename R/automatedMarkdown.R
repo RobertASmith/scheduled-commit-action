4496b4e63efc42b522d441b9583a7eee96f75768
Robert Smith <rasmith3@sheffield.ac.uk>
2021-10-25 17:48:33 +0100

    minor change

--- R/automatedMarkdown.R
+++ R/automatedMarkdown.R
@@ -1,8 +1,8 @@
 # Automated Rmarkdown creation & online publication...
-rmarkdown::render('./R/example.Rmd')
+#rmarkdown::render('./R/example.Rmd')
 
 markdown::rpubsUpload(id = "https://api.rpubs.com/api/v1/document/826791/13f0b53ed9244b559e2fd7902da114fd",
                       title='example',
-                      htmlFile="./R/example.html",
+                      htmlFile="R/example.html",
                       method=getOption('rpubs.upload.method','auto'))
                       
