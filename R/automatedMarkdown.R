7a25a8e42875504803c0691746c802d6a4e4776d
Robert Smith <41961614+RobertASmith@users.noreply.github.com>
2021-10-25 20:22:33 +0100

    Update automatedMarkdown.R

--- R/automatedMarkdown.R
+++ R/automatedMarkdown.R
@@ -1,8 +1,14 @@
 # Automated Rmarkdown creation & online publication...
-rmarkdown::render('R/example.Rmd')
+rmarkdown::render(output_dir = "plots", 
+                  output_format = "pdf_document",
+                  input = './R/example.Rmd')
+
+rmarkdown::render(output_dir = "plots", 
+                  output_format = "html_document",
+                  input = './R/example.Rmd')
 
 markdown::rpubsUpload(id = "https://api.rpubs.com/api/v1/document/826791/13f0b53ed9244b559e2fd7902da114fd",
-                      title='example',
+                      title='example', 
                       htmlFile="R/example.html",
                       method=getOption('rpubs.upload.method','auto'))
                       
